c70e961380e99dbbd2edc54c0a912727a5dcff77
Ruhama935 <r49935@gmail.com>
2025-03-20 03:13:38 +0200

    Starting a project - first step

---
+++ PaintingsShop/Controllers/UsersController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PaintingsShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        string filePath = "./userFile.txt";
+        // GET: api/<UsersController>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+
+            List<User> users = new List<User>();
+            using (StreamReader reader = System.IO.File.OpenText(filePath))
+            {
+                string? currentUserInFile;
+                while ((currentUserInFile = reader.ReadLine()) != null)
+                {
+                    User user = JsonSerializer.Deserialize<User>(currentUserInFile);
+                    users.Add(user);
+                }
+                return Ok(users);
+            }
+
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public ActionResult<User> GetById(int id)
+        {
+            using (StreamReader reader = System.IO.File.OpenText(filePath))
+            {
+                string? currentUserInFile;
+                while ((currentUserInFile = reader.ReadLine()) != null)
+                {
+                    User user = JsonSerializer.Deserialize<User>(currentUserInFile);
+                    if (user.Id == id)
+                        return Ok(user);
+                }
+            }
+            return NoContent();
+
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public IActionResult SignUp([FromBody] User user)
+        {
+            int numberOfUsers = System.IO.File.ReadLines(filePath).Count();
+            user.Id = numberOfUsers + 1;
+            string userJson = JsonSerializer.Serialize(user);
+            System.IO.File.AppendAllText(filePath, userJson + Environment.NewLine);
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public ActionResult<User> Login(User user)
+        {
+            using (StreamReader reader = System.IO.File.OpenText(filePath))
+            {
+                string? currentUserInFile;
+                while ((currentUserInFile = reader.ReadLine()) != null)
+                {
+                    User currentUser = JsonSerializer.Deserialize<User>(currentUserInFile);
+                    if (user.userName == currentUser.userName && user.password == currentUser.password)
+                        return Ok(currentUser);
+                }
+            }
+            return NotFound();
+
+        }
+
+        // PUT api/<UsersController>/5
+        //[HttpPut("{id}")]
+        //public ActionResult<User> Put(int id, [FromBody] User user)
+        //{
+        //    string textToReplace = string.Empty;
+        //    using (StreamReader reader = System.IO.File.OpenText(filePath))
+        //    {
+        //        string currentUserInFile;
+        //        while ((currentUserInFile = reader.ReadLine()) != null)
+        //        {
+
+        //            User Cuser = JsonSerializer.Deserialize<User>(currentUserInFile);
+        //            if (Cuser.Id == id)
+        //                textToReplace = currentUserInFile;
+        //        }
+        //    }
+
+        //    if (textToReplace != string.Empty)
+        //    {
+        //        string text = System.IO.File.ReadAllText(filePath);
+        //        text = text.Replace(textToReplace, JsonSerializer.Serialize(user));
+        //        System.IO.File.WriteAllText(filePath, text);
+        //    }
+
+
+        //}
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PaintingsShop/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaintingsShop
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string userName { get; set; }
+        [Required]
+        public string password { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+
+    }
+}
