0e2bacdf8865012090f0003e1620577ff0856484
Ruhama935 <r49935@gmail.com>
2025-04-22 00:13:27 +0300

    Organization according to the stratification method

--- PaintingsShop/Controllers/UsersController.cs
+++ PaintingsShop/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
+//using Entities;
+using Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,98 +11,56 @@ namespace PaintingsShop.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        string filePath = "./userFile.txt";
+        UserService userService = new UserService();
+
         // GET: api/<UsersController>
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-
-            List<User> users = new List<User>();
-            using (StreamReader reader = System.IO.File.OpenText(filePath))
-            {
-                string? currentUserInFile;
-                while ((currentUserInFile = reader.ReadLine()) != null)
-                {
-                    User user = JsonSerializer.Deserialize<User>(currentUserInFile);
-                    users.Add(user);
-                }
+            List<User> users = userService.GetUser();
+            if (users.Count > 0)
                 return Ok(users);
-            }
-
+            return NoContent();
         }
 
         // GET api/<UsersController>/5
         [HttpGet("{id}")]
         public ActionResult<User> GetById(int id)
         {
-            using (StreamReader reader = System.IO.File.OpenText(filePath))
-            {
-                string? currentUserInFile;
-                while ((currentUserInFile = reader.ReadLine()) != null)
-                {
-                    User user = JsonSerializer.Deserialize<User>(currentUserInFile);
-                    if (user.Id == id)
-                        return Ok(user);
-                }
-            }
-            return NoContent();
-
+            User user = userService.getUserByID(id);
+            if (user == null)
+                return NoContent();
+            return Ok(user);
         }
 
         // POST api/<UsersController>
         [HttpPost]
         public IActionResult SignUp([FromBody] User user)
         {
-            int numberOfUsers = System.IO.File.ReadLines(filePath).Count();
-            user.Id = numberOfUsers + 1;
-            string userJson = JsonSerializer.Serialize(user);
-            System.IO.File.AppendAllText(filePath, userJson + Environment.NewLine);
-            return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
+            User newUser = userService.SignUp(user);
+            if (user == null)
+                return BadRequest();
+            return Ok(user);
         }
 
         [Route("login")]
         [HttpPost]
         public ActionResult<User> Login(User user)
         {
-            using (StreamReader reader = System.IO.File.OpenText(filePath))
-            {
-                string? currentUserInFile;
-                while ((currentUserInFile = reader.ReadLine()) != null)
-                {
-                    User currentUser = JsonSerializer.Deserialize<User>(currentUserInFile);
-                    if (user.userName == currentUser.userName && user.password == currentUser.password)
-                        return Ok(currentUser);
-                }
-            }
-            return NotFound();
-
+            User newUser = userService.Login(user);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
         }
 
         //PUT api/<UsersController>/5
         [HttpPut("{id}")]
         public ActionResult<User> Put(int id, [FromBody] User user)
         {
-            string textToReplace = string.Empty;
-            using (StreamReader reader = System.IO.File.OpenText(filePath))
-            {
-                string currentUserInFile;
-                while ((currentUserInFile = reader.ReadLine()) != null)
-                {
-
-                    User Cuser = JsonSerializer.Deserialize<User>(currentUserInFile);
-                    if (Cuser.Id == id)
-                        textToReplace = currentUserInFile;
-                }
-            }
-
-            if (textToReplace != string.Empty)
-            {
-                string text = System.IO.File.ReadAllText(filePath);
-                text = text.Replace(textToReplace, JsonSerializer.Serialize(user));
-                System.IO.File.WriteAllText(filePath, text);
-                return Ok(user);
-            }
-            return NotFound();
+            User newUser = userService.update(id, user);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
 
         }
 
---
+++ Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Repositories;
+
+namespace Services
+{
+    public class UserService
+    {
+        UserRepository repository = new UserRepository();
+        public List<User> GetUser()
+        {
+            return repository.GetUser();
+        }
+
+        public User getUserByID(int id)
+        {
+            return repository.getUserByID(id);
+        }
+
+        public User SignUp(User user)
+        {
+            return repository.SignUp(user);
+        }
+
+        public User Login(User user)
+        {
+            return repository.Login(user);
+        }
+
+        public User update(int id, User user)
+        {
+            return repository.update(id, user);
+        }
+
+    }
+}
